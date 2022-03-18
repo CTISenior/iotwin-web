7e78588f111176971398d6ca8fee9bf8dc3aac8f
Erdem Sert <erdem.sert@ug.bilkent.edu.tr>
2022-03-18 14:15:36 +0300

    notificationList code fixed

--- src/components/NotificationList.js
+++ src/components/NotificationList.js
@@ -2,22 +2,33 @@ import React from 'react';
 import Menu from '@mui/material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Fade from '@mui/material/Fade';
-import Divider from '@mui/material/Divider';
 import ListItemText from '@mui/material/ListItemText';
 import ErrorIcon from '@mui/icons-material/Error';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import WarningIcon from '@mui/icons-material/Warning';
 import DeleteIcon from '@mui/icons-material/Delete';
-
+import axios from 'axios';
 
 const NotificationList = (props)=>{
    const {anchorEl,open,handleClose,value,createdTime}=props;
    const heatValue=parseInt(value);
+   const notificationListItem = [
+    {
+        message: "The temperature is reached "+ heatValue +" degree",
+        createdTime: createdTime,
+        status:"error",
+    },
+    {
+      message: "The temperature is reached "+ heatValue +" degree",
+      createdTime: createdTime,
+      status:"warning",
+    }
+];
    const handleDelete=()=>{
-      
+      //axios.post("http://localhost:9090/api/v1/alerts",{notification})
    }
     return (
-        <Menu
+      <Menu
         id="fade-menu"
         MenuListProps={{
           'aria-labelledby': 'fade-button',
@@ -27,31 +38,26 @@ const NotificationList = (props)=>{
         onClose={handleClose}
         TransitionComponent={Fade}
       >
-        <MenuItem>
-          <ListItemIcon>
-            <ErrorIcon fontSize="medium" color="error"/>
-          </ListItemIcon>
-            <div style={{flexDirection:'column',margin:10}}>
-                <ListItemText>The temperature is reached {heatValue} degree</ListItemText>
-                <ListItemText style={{color:'gray'}}>{createdTime} minutes ago</ListItemText>
-            </div>
-            <ListItemIcon onClick={handleDelete}>
-            <DeleteIcon fontSize="medium"/>
-          </ListItemIcon>
-        </MenuItem>
-        <Divider sx={{ my: 0.5 }} />
-        <MenuItem>
-        <ListItemIcon>
-            <WarningIcon fontSize="medium" color="warning"/>
-          </ListItemIcon>
-            <div style={{flexDirection:'column',margin:10}}>
-                <ListItemText>The temperature is reached {heatValue} degree</ListItemText>
-                <ListItemText style={{color:'gray'}}>{createdTime} minutes ago</ListItemText>
-            </div>
-            <ListItemIcon onClick={handleDelete}>
-            <DeleteIcon fontSize="medium"/>
+      {notificationListItem.map((list) => (
+        <MenuItem key={list.message}>
+           {list.status==='error'?(
+             <ListItemIcon>
+                <ErrorIcon fontSize="medium" color="error"/>
+            </ListItemIcon>
+           ):(
+            <ListItemIcon>
+              <WarningIcon fontSize="medium" color="warning"/>
+            </ListItemIcon>
+           )}
+           <div style={{flexDirection:'column',margin:10}}>
+                <ListItemText>{list.message}</ListItemText>
+                <ListItemText style={{color:'gray'}}>{list.createdTime}</ListItemText>
+           </div>
+           <ListItemIcon onClick={handleDelete}>
+              <DeleteIcon fontSize="medium"/>
           </ListItemIcon>
         </MenuItem>
+    ))}
       </Menu>
     )
 }
